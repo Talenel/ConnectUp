e2b79f369575b19c1b6e6035de532c826cfe27ea
Justin Gross <jmbg144@gmail.com>
2017-06-30 16:01:35 -0400

    Initial Custom Security Config implemented

---
+++ src/main/java/com/example/demo/configs/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.example.demo.configs;
+
+/**
+ * Created by student on 6/30/17.
+ */
+import com.example.demo.directories.UserRepository;
+import com.example.demo.services.SSUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired private UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsServiceBean( ) throws Exception{
+        return new SSUserDetailsService(userRepository);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/register","/assets/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceBean());
+
+
+    }
+}
